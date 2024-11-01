d25cdd73758c7edc26784e4bbd6b5f45287f1574
Helmiat <miasaritam@gmail.com>
2024-11-01 20:44:48 +0800

    Add files via upload

---
+++ ALGROTMA PROGRAM PROG1.PY D.py
@@ -0,0 +1,8 @@
+ # buatlah Algoritma program output menampilkan biodata mahasiswa sebagai berikut
+Print(Nama saya helmiati)
+Print(nim saya 242421120)
+Print(asal saya,Sarita,Dasa punti,Kec.soromandi,kab.Bima)
+Print(nomor Nim pertama=24)
+print(nomor Nim terakhir=20)
+print (Nomor keberuntungan saya adalah 24+20)
+print=(44)
