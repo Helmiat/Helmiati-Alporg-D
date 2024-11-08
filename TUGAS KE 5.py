609a24b0970ecb0c11b263e059a1bab70095c825
Helmiat <miasaritam@gmail.com>
2024-11-08 13:39:18 +0800

    Add files via upload

---
+++ TUGAS KE 5.py
@@ -0,0 +1,7 @@
+Prog1.py variabel assigment value
+print("penugasan nilai ke variabel) 
+print( int x=10://integer,float=5.5 ://float, float z=x+y;//operasi penjumlahan,jadi         
+print("nilai  x adalah :10")
+print("nilai y adalah:5.5") 
+print("nilai z adalah :15,5") 
+print("15.5")
