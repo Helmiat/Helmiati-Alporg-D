6bacd8699d3209f75a88e77aa7930faee5ecea68
Helmiat <miasaritam@gmail.com>
2024-11-17 12:48:46 +0800

    Add files via upload

---
+++ TUGAS KE 6 ALGORITMA PROGRAM.py
@@ -0,0 +1,12 @@
+Prog1.py modifikasi tugas ke 2 program Python
+def hitung_luas(panjang,lebar):"""menghitung luas persegi panjang."""
+return panjang*lebar
+#Validasi nim
+print def ekstrak_nim:("24241120")
+print validasi nim memiliki 8 digit dan jumlah angkahnya genap
+print (nim)==8 and nim.isdigit(24241120):jumlah_angkah=8
+print ekstrak_panjang,lebar,luas persegi
+print panjang=("2424")   
+print lebar=("1120")
+print L=(PXL=2424X1120)
+print luas persegi panjang L("=2.714.880")
