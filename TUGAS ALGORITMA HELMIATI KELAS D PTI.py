299d00f249e638adfeca06cd2cee93ea5b07a765
Helmiat <miasaritam@gmail.com>
2024-12-05 17:41:33 +0800

    Add files via upload

---
+++ TUGAS ALGORITMA HELMIATI KELAS D PTI.py
@@ -0,0 +1,34 @@
+#ARITMATIKA(X:+-**%//)
+a=24 # 2 digit nim awal
+b=20 # 2digit nim akhir
+
+#perkalian
+hasil=a x b
+print(nilai hperkalian a x b=,480)
+
+#pembagian
+hasil a:b
+print("nilai dari pembagian a:b=1,2")
+
+# Penjumlahan 
+hasil= a+b
+print("nilai dari a+b=,44")
+
+#penguranngan
+hasil=a-b
+print(nilai dari pengurangan a-b=,4")
+
+#perpangkatan
+hasil=a**b
+print("nilai dari a pangkat b")
+("24²=24x24=576,")
+("20²=20x20=400,")
+
+#modulus(sisa bagi)
+hasil=a % b 
+print("nilai dari a amod b=4,")
+
+#flot dividio(pembagian terdekat-kebawah)
+hasil=a//b
+("jadi,24//20")
+print("nilai dari a flot b=1")
