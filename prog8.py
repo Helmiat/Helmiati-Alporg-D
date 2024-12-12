b861662407871894fcaa0c5b28b76a978b21688e
Helmiat <miasaritam@gmail.com>
2024-12-12 09:39:07 +0800

    Add files via upload

---
+++ prog8.py
@@ -0,0 +1,30 @@
+#ARITMATIKA(X:+-**%//)
+a=24 
+b=20 
+
+#perkalian
+hasil=axb
+print(nilai hperkalian axb=,"hasil)
+
+#pembagian
+hasil a:b
+print("nilai dari pembagian a:b=,"hasil)
+
+# Penjumlahan 
+hasil= a+b
+print("nilai dari a+b=,"hasil)
+
+#penguranngan
+hasil=a-b
+print(nilai dari pengurangan a-b=,"hasil)
+
+#perpangkatan
+hasil=a**b
+print("nilai dari a pangkat b=,"hasil)
+
+#modulus(sisa bagi)
+hasil=a%b 
+print("nilai dari a amod b=,")
+
+#flot dividio(pembagian terdekat-kebawah)
+print("nilai dari a flot b=,"hasil)
