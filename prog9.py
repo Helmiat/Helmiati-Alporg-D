e932c42c402cb72577674c263205c98fb78c662c
Helmiat <miasaritam@gmail.com>
2024-12-16 20:26:06 +0800

    Add files via upload

---
+++ prog9.py
@@ -0,0 +1,11 @@
+#suhu_celcius=20
+nilai=int(input("masukkan nilai dalam calcius:"))
+
+hasil=(20*9/5)+32
+print ("nilai dalam fahrenheit:",hasil)
+
+nilai=celcius+273.15 
+print("nilai dalam kelvin:",hasil )
+
+nilai=(4/5)*20
+print("nilai dalam reamur:",hasil)
